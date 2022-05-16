ac92c84c8f2a5acd6a9e1fc34fb032517db13916
lenryk <lenry101@gmail.com>
2022-05-16 22:24:50 +0100

    Exercise 2.05

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react'
+import './App.css'
 
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            passwordConfirmation: '',
+            email: '',
+            errors: {},
+        }
 
-    state = {
-        username: '',
-        password: '',
-        passwordConfirmation: '',
-        email: '',
-        errors: []
+        this.validateUsernameOnBlur = this.validateUsernameOnBlur.bind(this)
+        this.validatePasswordOnBlur = this.validatePasswordOnBlur.bind(this)
+        this.validateEmailOnBlur = this.validateEmailOnBlur.bind(this)
+        this.validatePasswordConfirmationOnBlur = this.validatePasswordConfirmationOnBlur.bind(this)
     }
+
 
 
     submitForm(event) {
@@ -18,17 +27,64 @@ class App extends Component {
     }
 
     validateUsernameOnBlur = (event) => {
-        console.log(event.target.value)
-        this.setState()
+        const username = event.target.value;
+        const errors = this.state.errors;
+        errors.username = this.validateNotEmpty('Username', username)
+        this.setState({username, errors})
+    }
+
+    validatePasswordOnBlur = (event) => {
+        const password = event.target.value;
+        const errors = this.state.errors;
+        errors.password = this.validateNotEmpty('Password', password)
+        this.setState({password, errors})
+    }
+
+    validateEmailOnBlur = (event) => {
+        const email = event.target.value;
+        const errors = this.state.errors;
+        errors.email = this.validateNotEmpty('Email', email)
+        this.setState({email, errors})
+    }
+
+    validateEmailFormat(fieldName, value) {
+        let [lhs, rhs] = value.split("@")
+        lhs = lhs || ' '
+        rhs = rhs || ' '
+        if (lhs.length <= 0 || rhs.length <=0) return `${fieldName} must be in a standard email format`
+    }
+
+    validatePasswordConfirmationOnBlur(event) {
+        const passwordConfirmation = event.target.value;
+        const errors = this.state.errors;
+        if (passwordConfirmation !== this.state.password) {
+            errors.passwordMatch = 'Passwords must match password confirmation.'
+        }
+
+        this.setState({passwordConfirmation, errors})
+    }
+
+    validateNotEmpty(fieldName, value) {
+        if (value.length <= 0) {
+            return `${fieldName} must be filled out`
+        }
+    }
+
+    displayErrors() {
+        return (
+            <div className='errors'>
+                {Object.values(this.state.errors).map((err, i) => <p key={`err-${i}`}>{err}</p>)}
+            </div>
+        )
     }
 
     displayForm() {
         return (
             <div>
                 Username: <input type='text' onBlur={this.validateUsernameOnBlur}/><br/>
-                Password: <input type='text' /><br/>
-                Password Confirmation: <input type='text' /><br />
-                Email: <input type='text' /><br />
+                Password: <input type='text' onBlur={this.validatePasswordOnBlur}/><br/>
+                Password Confirmation: <input type='text' onBlur={this.validatePasswordConfirmationOnBlur}/><br />
+                Email: <input type='text' onBlur={this.validateEmailOnBlur}/><br />
                 <br />
                 <button onClick={this.submitForm}>Submit</button>
             </div>
@@ -39,6 +95,7 @@ class App extends Component {
     return (
         <div className='App'>
          Create Account
+            {this.displayErrors()}
             <hr />
             {this.displayForm()}
         </div>
