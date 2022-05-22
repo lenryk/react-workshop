e6400978db6842882872cb5c4c8e9c5d16bbada2
lenryk <lenry101@gmail.com>
2022-05-22 20:36:15 +0100

    Exercise 6.02

---
+++ modal-example/src/Components/Modal/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class Modal extends Component {
+    render() {
+        if (!this.props.showModal) {
+            return null;
+        }
+        return (
+            <div className="modal">
+                <div className="modal_content">
+                    {this.props.children}
+                    <div>
+                        <button onClick={this.props.toggleModal}>Close</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export { Modal };
