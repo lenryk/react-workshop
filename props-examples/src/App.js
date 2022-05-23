eb056f1f8655032c61f1354700da0c0971250db9
lenryk <lenry101@gmail.com>
2022-05-23 22:34:40 +0100

    Exercise 7.06

--- props-examples/src/App.js
+++ props-examples/src/App.js
@@ -7,22 +7,25 @@ class App extends Component {
               name: 'Tiger',
               number: 3890,
               endangered: true,
-              photo: 'https://source.unsplash.com/S0txA-JnUFA/200x100',
+              photo: 'https://source.unsplash.com/S0txA-JnUFA/400x300',
               id: 1,
+              donation: 100,
           },
           {
               name: 'Brown Bear',
               number: 200000,
               endangered: false,
-              photo: 'https://source.unsplash.com/c8XlAc1akIU/200x100',
+              photo: 'https://source.unsplash.com/c8XlAc1akIU/400x300',
               id: 2,
+              donation: 10,
           },
           {
               name: 'Red Panda',
               number: 10000,
               endangered: true,
-              photo: 'https://source.unsplash.com/2zYHKx8jtvU/200x100',
+              photo: 'https://source.unsplash.com/2zYHKx8jtvU/400x300',
               id: 3,
+              donation: 50,
           }
       ];
 
@@ -43,7 +46,7 @@ export class Animal extends Component {
               {this.props.children}
               <ul>
                   {details.map((detail, index) => (
-                      <AnimalDetails image={<Photo path={detail.photo} title={detail.name} />} detail={detail} key={index} />
+                      <WrapperComponent image={<Photo path={detail.photo} title={detail.name} />} detail={detail} key={index} />
                   ))}
               </ul>
           </div>
@@ -53,9 +56,10 @@ export class Animal extends Component {
 
 class AnimalDetails extends Component {
     render() {
-        const {name, number, endangered, id} = this.props.detail;
-        const {image} = this.props
+        const {name, number, endangered, id, donation} = this.props.detail;
+        const {image, donationColor} = this.props
 
+        console.log(donation)
         return (
             <li key={id}>
                 <div>
@@ -63,6 +67,9 @@ class AnimalDetails extends Component {
                     <p>Animal: {name}</p>
                     <p>Number: {number}</p>
                     <p>Endangered: {endangered ? 'Yes' : 'No'}</p>
+                    <p style={{ color: donationColor }}>
+                        Donation amount: <span className='donation-color'>${donation}</span>
+                    </p>
                 </div>
             </li>
         )
@@ -77,6 +84,28 @@ class Photo extends Component {
         )
     }
 }
+
+
+const withDonationColor = WrapperComponent => {
+    return class extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {donationColor: 'black'}
+        }
+
+        componentDidMount() {
+            const donationAmount = this.props.donation;
+            const donationColor = donationAmount > 50 ? 'green' : 'red'
+            this.setState({donationColor})
+        }
+
+        render() {
+            return <WrapperComponent {...this.props} donationColor={this.state.donationColor} />
+        }
+    }
+}
+
+const WrapperComponent = withDonationColor(AnimalDetails)
 
 
 export default App;
