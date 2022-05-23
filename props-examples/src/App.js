358ee2871aeed8b030e2b93af44358026678a0d5
lenryk <lenry101@gmail.com>
2022-05-23 21:06:36 +0100

    Exercise 7.02

--- props-examples/src/App.js
+++ props-examples/src/App.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    const details = {
-      name: 'Tiger',
-      number: 3890,
-      endangered: true
-    };
-    return <Animal details={details} />;
+    return (
+        <Animal name="Tiger" number={3890} endangered={true}>
+            <h1>Endangered Animals</h1>
+        </Animal>
+    )
   }
 }
 
 export class Animal extends Component {
   render() {
-    const { name, number, endangered } = this.props.details;
+    const { name, number, endangered, children } = this.props;
 
     return (
         <div>
+            {children}
           <p>Animal: {name}</p>
           <p>Number: {number}</p>
           <p>Endangered: {endangered ? 'Yes' : 'No'}</p>
